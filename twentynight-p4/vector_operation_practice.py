22fcab3ea7c64e6a66a91912e04e3b887999d483
Chengpeng Wang <chpengwang@gmail.com>
2018-06-21 06:19:22 +0800

    create class Vector

--- twentynight-p4/vector_operation_practice.py
+++ twentynight-p4/vector_operation_practice.py
@@ -1,4 +1,9 @@
 import math
+from decimal import Decimal, getcontext
+
+# 设置小数点后保留的位数
+getcontext().prec = 19
+TOLERANCE = 1e-10
 
 class Vector(object):
   # 实例（instance）的创建
@@ -6,7 +11,7 @@ class Vector(object):
     try:
       if not coordinates:
           raise ValueError
-      self.coordinates = tuple(coordinates)
+      self.coordinates = tuple([Decimal(x) for x in coordinates])
       self.dimension = len(coordinates)
 
     except ValueError:
@@ -39,12 +44,12 @@ class Vector(object):
     return sum(new_coordinates)
 
   def magnitude(self):
-    return math.sqrt(self.dot_product_with(self))
+    return Decimal(math.sqrt(self.dot_product_with(self)))
 
   def normalization(self):
     try:
       mag = self.magnitude()
-      return self.times_scalar(1/mag)
+      return self.times_scalar(Decimal(1)/mag)
     except ZeroDivisionError:
       #print("You can't normalize Zero Vector!")
       raise Exception("You can't normalize Zero Vector!")
@@ -63,13 +68,137 @@ class Vector(object):
         #print("At least one of the vector is Zero Vector! No angle defined.")
         raise Exception("One of the vector is Zero Vector! No angle defined.")
 
+  def __is_zero_vector(self):
+    return self.magnitude() < TOLERANCE
   
+  def is_parallel_to(self, v):
+    if v.__is_zero_vector() or self.__is_zero_vector():
+      return True
+    else:
+      s_m = self.normalization()
+      v_m = v.normalization()
+      return s_m.minus(v_m).__is_zero_vector() or s_m.plus(v_m).__is_zero_vector() 
+
+  def is_orthogonal_to(self, v):
+    return abs(self.dot_product_with(v)) < TOLERANCE
+
+  def component_parallel_to(self, base_vactor):
+    u_b = base_vactor.normalization()
+    mag = self.dot_product_with(u_b)
+    return u_b.times_scalar(mag)
+
+  def component_orthogonal_to(self, base_vactor):
+    c_p = self.component_parallel_to(base_vactor)
+    return self.minus(c_p)
+
+  
+  def cross_product_with(self, v):
+    if self.dimension != 3 or v.dimension != 3:
+      raise Exception("Both cross product vectors must be three dimensional")
+    
+    if self.is_parallel_to(v):
+      return Vector([0 for i in range(self.dimension)])
+    else:
+      x = self.coordinates[1]*v.coordinates[2] - self.coordinates[2]*v.coordinates[1]
+      y = self.coordinates[2]*v.coordinates[0] - self.coordinates[0]*v.coordinates[2]
+      z = self.coordinates[0]*v.coordinates[1] - self.coordinates[1]*v.coordinates[0]
+      return Vector([x, y, z])
+
+  def area_of_parallelogram_spanned_with(self, v):
+    new_self = self
+    new_v = v
+    if self.dimension == 2:
+      new_self = Vector(self.coordinates + (0,))
+    if v.dimension == 2:
+      new_v = Vector(v.coordinates + (0,))
+
+    return new_self.cross_product_with(new_v).magnitude()
+  
+  def area_of_triangle_spanned_with(self, v):
+    return self.area_of_parallelogram_spanned_with(v) / Decimal(2)
 
 
 
-v5 = Vector([-8.802, 6.776])
-print(v5.magnitude())
-print(v5.dot_product_with(v5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+v0 = Vector([5, 3, -2])
+u0 = Vector([-1, 0, 3])
+print(v0.cross_product_with(u0))
+
+v1 = Vector([8.462, 7.893, -8.187])
+v2 = Vector([6.984, -5.975, 4.778])
+print(v1.cross_product_with(v2))
+
+v3 = Vector([-8.987, -9.838, 5.031])
+v4 = Vector([-4.268, -1.861, -8.866])
+print(v3.area_of_parallelogram_spanned_with(v4))
+
+v5 = Vector([1.5, 9.547, 3.691])
+v6 = Vector([-6.007, 0.124, 5.772])
+print(v5.area_of_triangle_spanned_with(v6))
+'''
+
+'''
+v1 = Vector([3.039, 1.879])
+base_1 = Vector([0.825, 2.036])
+
+v2 = Vector([-9.88, -3.264, -8.159])
+base_2 = Vector([-2.155, -9.353, -9.473])
+
+v3 = Vector([3.009, -6.172, 3.692, -2.51])
+#v3 = Vector([0, 0, 0, 0])
+base_3 = Vector([6.404, -9.144, 2.759, 8.718])
+#base_3 = Vector([0, 0, 0, 0])
+
+print(v1.component_parallel_to(base_1))
+print(v2.component_orthogonal_to(base_2))
+print(v3.component_parallel_to(base_3))
+print(v3.component_orthogonal_to(base_3))
+'''
+
+
+
+'''
+v1 = Vector([-7.579, -7.88])
+v2 = Vector([22.737, 23.64])
+
+v3 = Vector([-2.029, 9.97, 4.172])
+v4 = Vector([-9.231, -6.639, -7.245])
+
+v5 = Vector([-2.328, -7.284, -1.214])
+v6 = Vector([-1.821, 1.072, -2.94])
+
+v7 = Vector([2.118, 4.827])
+v8 = Vector([0, 0])
+
+
+print(v1.is_parallel_with(v2))
+print(v1.is_orthogonal_with(v2))
+
+print(v3.is_parallel_with(v4))
+print(v3.is_orthogonal_with(v4))
+
+print(v5.is_parallel_with(v6))
+print(v5.is_orthogonal_with(v6))
+
+print(v7.is_parallel_with(v8))
+print(v7.is_orthogonal_with(v8))
+
+'''
 
 
 '''
@@ -92,6 +221,7 @@ print(v5.angle_with(v6))
 print(v7.angle_with(v8, in_degrees=True))
 print(v5.angle_with(v5))
 '''
+
 
 '''
 v1 = Vector([-0.221, 7.437])
